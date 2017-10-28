8211a0643d47be44810906ab43a1471f7d69910d
jiaoqiqi <1176932165@qq.com>
2017-10-28 17:19:39 +0800

    binary with depth find
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/BottomLeftTree.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/find-bottom-left-tree-value/description/
+
+public class BottomLeftTree {
+    int h = 0;
+    int result = 0;
+
+    public int findBottomLeftValue(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return result;
+    }
+
+    public void findBottomLeftValue(TreeNode root, int depth) {
+        if (h < depth) {
+            result = root.val;
+            h = depth;
+        }
+        if (root.left != null) findBottomLeftValue(root.left, depth + 1);
+        if (root.right != null) findBottomLeftValue(root.right, depth + 1);
+    }
+
+    public static void main(String[] args) {
+        BottomLeftTree bottomLeftTree = new BottomLeftTree();
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(3);
+        t1.right = new TreeNode(2);
+        System.out.println(bottomLeftTree.findBottomLeftValue(t1));
+    }
+}
