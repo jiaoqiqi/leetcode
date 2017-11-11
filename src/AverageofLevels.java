606ec03a0be22748201297e43a75c6c38719cd96
jiaoqiqi <1176932165@qq.com>
2017-11-11 15:21:30 +0800

    计算树的深度
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/AverageofLevels.java
+++ src/AverageofLevels.java
@@ -31,7 +31,7 @@ public class AverageofLevels {
                  TreeNode node = q.poll();
                  sum += node.val;
                  if (node.left!=null){
-                     q.offer(node.left);
+                        q.offer(node.left);
                  }
                  if(node.right!=null){
                      q.offer(node.right);
---
+++ src/MaximumDepth.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepth {
+    public int maxDepth(TreeNode root) {
+        int dep = 0;
+        if (root==null){
+            return dep;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            dep++;
+            int n = q.size();  //某一行元素个数
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return dep;
+    }
+
+    public static void main(String []args){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        MaximumDepth maximumDepth = new MaximumDepth();
+        System.out.println(maximumDepth.maxDepth(root));
+
+    }
+
+}
