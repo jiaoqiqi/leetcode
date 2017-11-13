a203cc1017219992f6d2e1f55eb3aa6f7b8f4ee2
jiaoqiqi <1176932165@qq.com>
2017-11-13 15:46:38 +0800

    九宫格
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/MaxArea.java
@@ -0,0 +1,41 @@
+public class MaxArea {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max_area = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 1) {
+                    max_area = Math.max(max_area, AreaOfIsland(grid, i, j));
+                }
+        return max_area;
+    }
+
+    public int AreaOfIsland(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + AreaOfIsland(grid, i + 1, j) +
+                    AreaOfIsland(grid, i - 1, j) +
+                    AreaOfIsland(grid, i, j - 1) +
+                    AreaOfIsland(grid, i, j + 1);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        int[][] grid2 = {{0, 1, 0, 0, 0, 0, 0, 0}};
+        MaxArea maxArea = new MaxArea();
+        int res = maxArea.maxAreaOfIsland(grid);
+        System.out.println(res);
+    }
+
+
+}
