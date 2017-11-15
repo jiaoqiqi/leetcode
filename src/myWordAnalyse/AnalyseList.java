30c4fa69ac7442cd0d17ec2db8f3c2733e50b5b5
jiaoqiqi <1176932165@qq.com>
2017-11-15 19:50:15 +0800

    错的follo集
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/myWordAnalyse/AnalyseList.java
+++ src/myWordAnalyse/AnalyseList.java
@@ -10,8 +10,9 @@ public class AnalyseList {
 
     ArrayList<Production> productions = new ArrayList<Production>();
     ArrayList<String> nonTerminals = new ArrayList<String>();
-    ArrayList<String> terminals = new ArrayList<String >();
+    ArrayList<String> terminals = new ArrayList<String>();
     HashMap<String, ArrayList<String>> firsts = new HashMap<>();
+    HashMap<String, ArrayList<String>> follows = new HashMap<>();
 
     public void setProductions() {
         try {
@@ -89,138 +90,103 @@ public class AnalyseList {
 //        System.out.println(terminals);
     }
 
-//    public void setFirst() {
-//
-//        //终结符的first集
-//        for (int i = 0; i < terminals.size(); i++) {
-//            ArrayList first = new ArrayList();
-//            first.add(terminals.get(i));
-////            System.out.println(first);
-//            firsts.put(terminals.get(i),first);
-//        }
-////        System.out.println(firsts);
-//
-//        for (int i = 0; i < nonTerminals.size(); i++) {
-//            ArrayList first = new ArrayList<String>();
-//            firsts.put(nonTerminals.get(i), first);
-//        }
-////        System.out.println(firsts);
-//
-//        boolean flag;
-//        while(true){
-//            flag = true;
-//            String left;
-//            String right;
-//            String[] rights;
-//            for (int i = 0; i < productions.size(); i++) {
-//                left = productions.get(i).returnLeft();
-//                rights = productions.get(i).returnRights();
-//
-//                for (int j = 0; j < rights.length; j++) {
-//                    String s = rights[j];
-//                    right = rights[j];
-//                    System.out.println(s);
-//
-//                    if (!right.equals("$")) {
-////                        System.out.println(right);
-//                        for (int k = 0; k < right.length(); k++) {
-//                            String[] splitRight = right.split("");
-//                            for (int l = 0; l < splitRight.length; l++) {
-//                                if (firsts.get(left).contains(splitRight[l])){
-//                                    continue;
-//                                }
-//                                else{
-//                                    firsts.get(left).add(splitRight[i]);
-////                                    flag = false;
-//                                }
-//
-//                            }
-////                            if (firsts.get(left).contains(firsts.get(right).get(k))){
-////                                continue;
-////                            }else {
-////                                firsts.get(left).add(firsts.get(right).get(k));
-////                                flag = false;
-////                            }
-//
-//                        }
-////                        for (int k = 0; k < firsts.get(right).size(); k++) {
-////                            if (firsts.get(left).contains(firsts.get(right).get(k))) {
-////                                continue;
-////                            } else {
-////                                firsts.get(left).add(firsts.get(right).get(k));
-////                                flag = false;
-////                            }
-////                        }
-//                    }
-//                    if (isCanBeNull(right)){
-//                        continue;
-//                    }else{
-//                        break;
-//                    }
-//                }
-//            }
-//            if (flag == true){
-//                break;
-//            }
-//        }
-////        System.out.println(firsts);
-//    }
-//
-//    public boolean isCanBeNull(String symbol) {
-//        String[] rights;
-//        for (int i = 0; i < productions.size(); i++) {
-//            // 找到产生式
-//            if (productions.get(i).toString().equals(symbol)) {
-//                rights = productions.get(i).returnRights();
-//                if (rights[0].equals("$")) {
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-public void setFirst() {
-    //1.找到非终结符数组
-    //2.为数组每一个元素，求其First集
-    //  1.找到非终结符所对应的产生式 eg. E->aBc|b|De|$
-    //  2.遍历产生式右部 {aBc , b , De , $}
-    //  3.如果第一个字符是终结符 例如 aBc , b  则将a , b加入First(E);
-    //  4.如果第一个字符是非终结符 例如 De 则将First(D)加入First(E)
-    for (String s : nonTerminals){
-        firsts.put(s , GetFirst(s));
+    public void setFirst() {
+        //1.找到非终结符数组
+        //2.为数组每一个元素，求其First集
+        //  1.找到非终结符所对应的产生式 eg. E->aBc|b|De|$
+        //  2.遍历产生式右部 {aBc , b , De , $}
+        //  3.如果第一个字符是终结符 例如 aBc , b  则将a , b加入First(E);
+        //  4.如果第一个字符是非终结符 例如 De 则将First(D)加入First(E)
+        for (String s : nonTerminals) {
+            firsts.put(s, getFirst(s));
+        }
+        System.out.println(firsts);
     }
-    System.out.println(firsts);
-}
 
-    private ArrayList<String> GetFirst(String left){
+    private ArrayList<String> getFirst(String left) {
         //非终结符都是产生式左部
         //所以根据非终结符找到对应的产生式
-        Production p = FindProd(left);
+        Production p = findProd(left);
         ArrayList<String> first = new ArrayList<>();
         //遍历产生式的各个右部
-        for (String r : p.right){
+        for (String r : p.right) {
             //如果第一个字符是终结符  直接添加进去
             String symbol = String.valueOf(r.charAt(0));
-            if(terminals.contains(symbol)){ //看他的第一个字符是否在终结字符里边
+            if (terminals.contains(symbol)) { //看他的第一个字符是否在终结字符里边
                 first.add(symbol);
             } else { //如果是非终结符 则递归
-                first.addAll(GetFirst(symbol));
+                first.addAll(getFirst(symbol));
             }
         }
         return first;
     }
 
-    private Production FindProd(String left) {
+    private void setFollows(){
+        for (String s : nonTerminals) {
+            follows.put(s, getFollow(s));
+        }
+//        System.out.println(follows);
+    }
+
+    private ArrayList<String> getFollow(String leftAtRight) {
+        ArrayList<String> follow = new ArrayList<>();
+        follow.add("#");
+        ArrayList LR = findNonTerminalAtRight(leftAtRight); //包含非终结符的右部的数组
+        for (int i = 0; i < LR.size(); i++) {
+            String containRight = LR.get(i).toString();  //每一个包含的本次要找的非终结符的字符串
+            String[] splitContain = containRight.split("");
+            if (!(leftAtRight.equals(splitContain[containRight.length() - 1]))) { //如果非终结符不在最后的位置
+                String willAdd = splitContain[containRight.indexOf(leftAtRight) + 1];  //就找到他的下一个字符
+
+                if (terminals.contains(willAdd)) { //如果是终结符直接加入follow集
+                    follow.add(willAdd);
+                } else {  //如果是非终结符，加入他除去$的first集
+                    ArrayList<String> firstWillAdd = getFirst(willAdd);
+                    System.out.println(firstWillAdd);
+                    for (int j = 0; j < firstWillAdd.size(); j++) {
+                        if (follow.contains(firstWillAdd.get(i))) {
+                            continue;
+                        } else if (!firstWillAdd.get(i).equals("$")) {
+                            follow.add(firstWillAdd.get(i));
+                        }
+
+                    }
+                }
+            }
+        }
+//        System.out.println(follow);
+        return follow;
+    }
+
+    private ArrayList<String> findNonTerminalAtRight(String LR) {
+        ArrayList<String> findRight = new ArrayList<>();
+        for (int i = 0; i < productions.size(); i++) {
+            String[] rights = productions.get(i).right;
+            for (int j = 0; j < rights.length; j++) {
+                String right = rights[j];
+//                System.out.println(right);
+                if (right.contains(LR)) {
+                    findRight.add(right);
+                }
+
+            }
+        }
+
+        return findRight;
+    }
+
+    private Production findProd(String left) {
         Production findLeft = productions.get(0);
         for (int i = 0; i < productions.size(); i++) {
-             if (productions.get(i).left.equals(left)){
-                 findLeft =  productions.get(i);
-                 break;
-             }
+            if (productions.get(i).left.equals(left)) {
+                findLeft = productions.get(i);
+                break;
+            }
         }
-        System.out.println(findLeft.left);
+//        System.out.println(findLeft.left);
         return findLeft;
     }
+
 
     public static void main(String[] args) {
         AnalyseList analyseList = new AnalyseList();
@@ -228,5 +194,9 @@ public void setFirst() {
         analyseList.setNonTerminals();
         analyseList.setTerminals();
         analyseList.setFirst();
+//        analyseList.findNonTerminalAtRight("F");
+//        analyseList.getFollow("E");
+        analyseList.setFollows();
+
     }
 }
