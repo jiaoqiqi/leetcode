fb25b319706c9f34f8ef0a19ff3dd3eb1bfc3931
jiaoqiqi <1176932165@qq.com>
2017-11-15 19:46:21 +0800

    leetcode
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/FindDuplicateFile.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/find-duplicate-file-in-system/description/
+
+import java.util.*;
+
+public class FindDuplicateFile {
+    public List<List<String>> findDuplicate(String[] paths) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        int n = paths.length;
+        if (n == 0) return result;
+
+        Map<String, Set<String>> map = new HashMap<>();
+        for (String path : paths) {
+            String[] strs = path.split("\\s+");
+            for (int i = 1; i < strs.length; i++) {
+                int idx = strs[i].indexOf("(");
+                String content = strs[i].substring(idx);
+                String filename = strs[0] + "/" + strs[i].substring(0, idx);
+                Set<String> filenames = map.getOrDefault(content, new HashSet<String>());
+                filenames.add(filename);
+                map.put(content, filenames);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key).size() > 1) {
+                result.add(new ArrayList<String>(map.get(key)));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] path = {"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"};
+        FindDuplicateFile findDuplicateFile = new FindDuplicateFile();
+        System.out.println(findDuplicateFile.findDuplicate(path));
+    }
+}
