61cc1f91fe3b4b560bae0f483da49cd5544c762e
jiaoqiqi <1176932165@qq.com>
2017-11-16 22:52:18 +0800

    明天补
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/MapSumPairs.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/map-sum-pairs/description/
+
+public class MapSumPairs {
+    public MapSumPairs() {
+
+    }
+
+    public void insert(String key, int val) {
+
+    }
+
+    public int sum(String prefix) {
+        int result=0;
+        return  result;
+
+    }
+}
