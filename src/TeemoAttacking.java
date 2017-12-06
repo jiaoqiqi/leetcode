d9f56bda47fed8dc7890a1c50f91b05146ebf736
jiaoqiqi <1176932165@qq.com>
2017-12-06 22:55:20 +0800

    teemo
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/TeemoAttacking.java
+++ src/TeemoAttacking.java
@@ -1,5 +1,9 @@
+//https://leetcode.com/problems/teemo-attacking/description/
+
 public class TeemoAttacking {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
+        if (timeSeries == null || timeSeries.length == 0 || duration == 0) return 0;
+
         int result = 0, start = timeSeries[0], end = timeSeries[0] + duration;
         for (int i = 1; i < timeSeries.length; i++) {
             if (timeSeries[i] > end) {
@@ -10,6 +14,7 @@ public class TeemoAttacking {
         }
         result += end - start;
 
+        System.out.println(result);
         return result;
     }
 
