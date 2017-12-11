7ee0e7625746c778bd52b09e84e3fc095a1989f6
jiaoqiqi <1176932165@qq.com>
2017-12-11 18:41:20 +0800

    map是个好的哦昂西
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/SortCharacters.java
+++ src/SortCharacters.java
@@ -10,6 +10,7 @@ public class SortCharacters {
                 map.put(c, 1);
             }
         }
+
         List<Character> [] bucket = new List[s.length() + 1];
         for (char key : map.keySet()) {
             int frequency = map.get(key);
@@ -17,6 +18,11 @@ public class SortCharacters {
                 bucket[frequency] = new ArrayList<>();
             }
             bucket[frequency].add(key);
+        }
+        for (int i = 0; i < bucket.length; i++) {
+            List<Character> characters = bucket[i];
+            System.out.println(characters);
+
         }
         StringBuilder sb = new StringBuilder();
         for (int pos = bucket.length - 1; pos >=0; pos--) {
