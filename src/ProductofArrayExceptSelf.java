90ccf1cbe7ec9d99b3a291e3fa98d8be01c66883
jiaoqiqi <1176932165@qq.com>
2017-12-22 13:24:09 +0800

    expect itself
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/ProductofArrayExceptSelf.java
+++ src/ProductofArrayExceptSelf.java
@@ -2,19 +2,71 @@
 
 public class ProductofArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+//        int n = nums.length;
+//        int[] res = new int[n];
+//        res[0] = 1;
+//        for (int i = 1; i < n; i++) {
+//            res[i] = res[i - 1] * nums[i - 1];
+//        }
+//
+//        int right = 1;
+//        for (int i = n - 1; i >= 0; i--) {
+//            res[i] *= right;
+//            right *= nums[i];
+//        }
+//        return res;
+        int[] result = new int[nums.length];
+        int count=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] ==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+        if (count>=2){
+            for (int i = 0; i < result.length; i++) {
+                result[i]=0;
+            }
+            for (int i = 0; i < result.length; i++) {
+                System.out.println(result[i]);
+            }
+            return  result;
         }
 
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            res[i] *= right;
-            right *= nums[i];
+        int mul = 1;
+        int mul0=1;
+
+        for (int i = 0; i < nums.length; i++) {
+            mul0*=nums[i];
         }
-        return res;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                mul *= nums[i];
+            }
+        }
+
+        if (mul==mul0){
+            for (int i = 0; i < result.length; i++) {
+                result[i] = mul / nums[i];
+            }
+        }
+        if(mul!=mul0){
+            for (int i = 0; i < result.length; i++) {
+                if (nums[i] ==0){
+                    result[i] = mul;
+                }else{
+                    result[i] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            int i1 = result[i];
+            System.out.println(i1);
+
+        }
+        return result;
     }
 
     public static void main(String[] args) {
