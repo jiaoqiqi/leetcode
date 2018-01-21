3dd62e1d8ffcbfecaaaaa31da622859841f28cf4
jiaoqiqi <1176932165@qq.com>
2018-01-21 13:01:33 +0800

    employee free time
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/EmployeeFreeTime.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/employee-free-time/description/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class EmployeeFreeTime {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> result = new ArrayList<>();
+
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
+        schedule.forEach(e -> pq.addAll(e));
+
+        Interval temp = pq.poll();
+        while(!pq.isEmpty()) {
+            if(temp.end < pq.peek().start) { // no intersect
+                result.add(new Interval(temp.end, pq.peek().start));
+                temp = pq.poll(); // becomes the next temp interval
+            }else { // intersect or sub merged
+                temp = temp.end < pq.peek().end ? pq.peek() : temp;
+                pq.poll();
+            }
+        }
+        return result;
+    }
+
+}
