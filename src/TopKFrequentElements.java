709979d542b45f6fe635346ba740b84e299be968
jiaoqiqi <1176932165@qq.com>
2018-01-30 16:13:04 +0800

    找出数组中前k个众数
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/TopKFrequentElements.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+        for (int num : nums){
+            frequencyMap.put(num,frequencyMap.getOrDefault(num,0)+1);
+        }
+
+//        System.out.println(frequencyMap);
+
+        for (int key : frequencyMap.keySet()){
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null){
+                    bucket[frequency] = new ArrayList<>();
+
+            }
+            bucket[frequency].add(key);
+
+        }
+//
+//        for (int i = 0; i < bucket.length; i++) {
+//            List<Integer> integers = bucket[i];
+//            System.out.println(integers);
+//
+//        }
+
+        for (int pos = bucket.length - 1; pos >= 0 && result.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                result.addAll(bucket[pos]);
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
+        int [] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(topKFrequentElements.topKFrequent(nums,k));
+    }
+}
