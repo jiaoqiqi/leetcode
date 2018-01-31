3425d8d2737d972089af4bb0527429ad64a72e58
jiaoqiqi <1176932165@qq.com>
2018-02-01 00:02:07 +0800

    add comment
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/FriendCircles.java
+++ src/FriendCircles.java
@@ -1,3 +1,8 @@
+//https://leetcode.com/problems/friend-circles/description/
+//DFS搜索，对于某个人，遍历其好友，然后再遍历其好友的好友，
+//就能把属于同一个朋友圈的人都遍历一遍，我们同时标记出已经遍历过的人，
+//然后累积朋友圈的个数，再去对于没有遍历到的人在找其朋友圈的人，这样就能求出个数
+
 public class FriendCircles {
     public void dfs(int[][] M, int[] visited, int i) {
         for (int j = 0; j < M.length; j++) {
